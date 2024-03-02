bb40879cbc7d6acce297204713bc1114c47bb207
Iryna Blazhenko <iryna.blazhenko@gmail.com>
2024-03-02 12:45:21 +0200

    Additional task3 added.

--- js_basic_task2/task3.js
+++ js_basic_task2/task3.js
@@ -13,3 +13,33 @@ Example
 Input validation
 If an empty value (null, None, Nothing, etc) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0
 */
+
+let array = [1, 0, NaN, 5, null, 54, 0, 54]
+
+let a = 5
+if (!Array.isArray(array)) {
+    console.log('This is not array ')
+} else if (array.length <= 2) {         //no need to check is the array empty
+    console.log('Array length is less or equal 2 elements')
+}
+if (array.includes(NaN)) {
+    console.log('The array contains "NaN" and the "NaN" will be stripped')
+}
+if (array.includes(null)) {
+    console.log('The array contains "null" and the NaN "null" be stripped')
+}
+
+let numberArray = []
+for (let i = 0; i < array.length; i++) {
+    if (Number(array[i]) || array[i] === 0) {
+        numberArray.push(array[i])
+    }
+}
+
+console.log(numberArray.sort((function (a, b) { return a - b })))  //0 - index is minimal value, last index - is the max value
+let sum = 0
+for (let i = 1; i < (numberArray.length - 1); i++) {     //only 1 min and 1 max value are excluded
+    sum += numberArray[i]
+}
+console.log(sum)
+
